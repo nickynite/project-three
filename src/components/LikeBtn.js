dd1ea8ca50f7c188804a6d0452c03388420f6d5e
nickynite <nbeullac@gmail.com>
2021-07-27 16:24:46 -0500

    fixed footer issue

--- src/components/LikeBtn.js
+++ src/components/LikeBtn.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import firebase from "firebase";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
